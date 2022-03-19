b879d80b98cd0febf8815b13616a68e7c5633dd6
tahabenk <tahabenkirane@gmail.com>
2022-03-19 13:16:40 +0100

    ok

---
+++ app/models/report_declaration.rb
@@ -0,0 +1,4 @@
+class ReportDeclaration < ApplicationRecord
+  belongs_to :report
+  belongs_to :declaration
+end
---
+++ db/migrate/20220319110107_create_reports.rb
@@ -0,0 +1,18 @@
+class CreateReports < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reports do |t|
+      t.datetime :accident_datetime
+      t.string :address
+      t.float :latitude
+      t.float :longtitude
+      t.boolean :flag_injury
+      t.boolean :flag_other_damage
+      t.string :visible_damages
+      t.string :observations
+      t.references :report_status, null: false, foreign_key: true
+      t.references :accident_picture, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
