cb71cad6c4221371edda48648ad4cc94fd6486c6
Baptiste Decarra <baptiste.decarra@gmail.com>
2022-03-22 18:57:01 +0100

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :insurance_policies
-  resources :insurance_certificates
-  resources :vehicles
-  resources :declarations
-  resources :reports
-  resources :drivers
-  devise_for :users
   root to: 'pages#home'
+  resources :drivers
+  resources :vehicles
+  resources :insurance_policies do
+    resources :insurance_certificates
+  end
+  resources :reports do
+    resources :declarations
+  end
+
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
