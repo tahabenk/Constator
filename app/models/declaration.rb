5be687a9511bc8102ea241209fc079bb346912f9
Baptiste Decarra <baptiste.decarra@gmail.com>
2022-03-30 10:16:06 +0000

    navbar revue et login/out options

--- app/models/declaration.rb
+++ app/models/declaration.rb
@@ -1,5 +1,5 @@
 class Declaration < ApplicationRecord
   belongs_to :insurance_policy
   belongs_to :report_declaration
-  has_one :report, through :report_declarations
+  has_one :report, through: :report_declarations
 end
