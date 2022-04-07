37bec652d05500a9118451814ec68e4c94b877cb
Baptiste Decarra <baptiste.decarra@gmail.com>
2022-04-07 15:26:40 +0000

    add signature

---
+++ app/javascript/packs/signature.js
@@ -0,0 +1,28 @@
+
+console.log("in signature.js")
+function resizeCanvas(canvas) {
+  var ratio = Math.max(window.devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+}
+
+$(document).on('turbolinks:load', function () {
+  var canvas = document.querySelector("canvas");
+  if (canvas) {
+    canvas.height = canvas.offsetHeight;
+    canvas.width = canvas.offsetWidth;
+    window.onresize = resizeCanvas(canvas);
+    resizeCanvas(canvas);
+    const signature_pad = new SignaturePad(canvas, { backgroundColor: '#F7F5F2' });
+    $('.signature_pad_clear').click(function () { signature_pad.clear() });
+    $('.signature_pad_save').click(function (event) {
+      if (signature_pad.isEmpty()) {
+        alert('You must sign before to save');
+        event.preventDefault();
+      } else {
+        $('.signature_pad_input').val(signature_pad.toDataURL());
+      }
+    });
+  }
+});
--- app/models/report.rb
+++ app/models/report.rb
@@ -7,5 +7,6 @@ class Report < ApplicationRecord
   has_many :drivers, through: :driver_reports
   has_many :vehicle_associations
   has_many :vehicles, through: :vehicle_associations
+  has_many_attached :signatures
 
 end
