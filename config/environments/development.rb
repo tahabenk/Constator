a90ff079851fd1d18c4b16607c00975beb3e6995
tahabenk <tahabenkirane@gmail.com>
2022-04-14 22:12:08 +0000

    FIX NEW REPORT DRAWing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :cloudinary
 
   config.active_storage.replace_on_assign_to_many = false
 
